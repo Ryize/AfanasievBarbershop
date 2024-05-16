55321e01dc8f5e65e560326d676ae0c38b60ad11
dimka2338 <liamsmit444@gmail.com>
2024-05-16 12:18:36 +0300

    feat: подключены файлы статики

--- AfanasievBarbershop/urls.py
+++ AfanasievBarbershop/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('administrator/', include('administrator.urls')),
-    path('profiles/', include('profiles.urls')),
+    path('', include('profiles.urls')),
 ]
