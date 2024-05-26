3bac475b65f08011ba30a9f3e3f96a5ad3d597ad
dimka2338 <liamsmit444@gmail.com>
2024-05-26 17:51:52 +0300

    feat: создана модель для мастера в которой отображаются его смены

--- masters/models.py
+++ masters/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from admins.models import Branch
+from authentication.models import CustomUser
+
+
+class WorkSchedule(models.Model):
+    master = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
+    date = models.DateField()
+    shift_morning = models.BooleanField(default=False)
+    shift_evening = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.master.username} - {self.date} - Morning: {self.shift_morning}, Evening: {self.shift_evening}'
