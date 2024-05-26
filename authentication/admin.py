fba2748817c7278f35352e5bdbb8fcf4226d2b3e
dimka2338 <liamsmit444@gmail.com>
2024-05-26 18:14:32 +0300

    feat: создана админка для создания и отображения пользователя

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('username', 'first_name', 'last_name', 'email', 'role', 'is_staff', 'is_active')
+    list_filter = ('role', 'is_staff', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'phone', 'role', 'branch', 'avatar')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'first_name', 'last_name', 'email',
+                       'phone', 'password1', 'password2', 'role',
+                       'branch', 'avatar', 'is_staff', 'is_active'
+                       )
+        }
+         ),
+    )
+    search_fields = ('email', 'username', 'first_name', 'last_name')
+    ordering = ('email', 'username')
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
