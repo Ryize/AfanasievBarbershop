e508fe06a88fc21d72ecc5ed4c7f5bf0f90ab131
dimka2338 <liamsmit444@gmail.com>
2024-05-29 17:31:30 +0300

    feat: создана страница всех мастеров

--- admins/urls.py
+++ admins/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+from admins.views import all_masters
+
 
 urlpatterns = [
+    path('all_masters/', all_masters, name='all_masters')
 ]
--- admins/views.py
+++ admins/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def all_masters(request):
+    return render(request, 'admins/admin_all_masters.html')
