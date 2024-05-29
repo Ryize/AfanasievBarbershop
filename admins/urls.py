bf18910e292b24e8217a8ffc1f54c7783866d380
dimka2338 <liamsmit444@gmail.com>
2024-05-29 17:41:43 +0300

    feat: создана страница c боковой страницей

--- admins/urls.py
+++ admins/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from admins.views import all_masters
+from admins.views import all_masters, left_panel
 
 
 urlpatterns = [
-    path('all_masters/', all_masters, name='all_masters')
+    path('all_masters/', all_masters, name='all_masters'),
+    path('left_panel/', left_panel, name='left_panel'),
 ]
--- admins/views.py
+++ admins/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def all_masters(request):
     return render(request, 'admins/admin_all_masters.html')
+
+
+def left_panel(request):
+    return render(request, 'admins/admin_base_with_left_panel.html')
