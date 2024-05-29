1345f16aa01a66d168b0b92626573526e1e11d5c
dimka2338 <liamsmit444@gmail.com>
2024-05-29 17:19:20 +0300

    feat: создана во views логика входа

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'authentication/index.html')
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('index')  # перенаправление на домашнюю страницу после успешного входа
+    else:
+        form = AuthenticationForm()
+    return render(request, 'authentication/login.html', {'form': form})
