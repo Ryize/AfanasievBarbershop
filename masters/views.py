20a9566084715cf3340c432136a46d7b75a0211a
Ezdakov Vladimir <ezdakov.vladimir@yandex.ru>
2024-06-04 22:29:42 -0700

    feat: masters: небольшие изменения

--- masters/views.py
+++ masters/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
+from admins.models import Branch
+
 
 def index(request):
     context = {
         'title': 'masters',
+        'branches': Branch.objects.all(),
     }
     return render(request, 'index.html', context)
