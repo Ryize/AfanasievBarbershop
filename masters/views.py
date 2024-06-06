de5d2b84a110d00ec2ecce16adb6030947c37ca2
Ezdakov Vladimir <ezdakov.vladimir@yandex.ru>
2024-06-06 04:51:55 -0700

    feat: masters: добавлены ссылки с календаря на расписание

--- masters/views.py
+++ masters/views.py
@@ -9,3 +9,13 @@ def index(request):
         'branches': Branch.objects.all(),
     }
     return render(request, 'index.html', context)
+
+
+def schedule(request, branch_id, date):
+    context = {
+        'title': 'schedule',
+        'branches': Branch.objects.all(),
+        'chairs': Branch.objects.get(id=branch_id).chairs,
+        'date': date,
+    }
+    return render(request, 'schedule.html', context)
