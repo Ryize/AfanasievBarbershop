45177323b68099da9e8da489a1bc9da837d49f9a
Ezdakov Vladimir <ezdakov.vladimir@yandex.ru>
2024-06-20 20:51:07 -0700

    refactor: настроены кнопки добавления и удаления графика

--- masters/views.py
+++ masters/views.py
@@ -1,8 +1,6 @@
 from datetime import datetime
 
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
-from django.http import JsonResponse
 from django.db import transaction
 
 from .models import Timetable, Branch
