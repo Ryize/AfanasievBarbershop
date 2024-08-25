1a1bb8239d6fa83406fc70132060ac7c49255d80
Ezdakov Vladimir <ezdakov.vladimir@yandex.ru>
2024-08-25 12:33:04 -0700

    fix: добавлены хосты

--- AfanasievBarbershop/settings.py
+++ AfanasievBarbershop/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = uuid.uuid4()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ryize-afanasievbarbershop-608f.twc1.net', 'www.ryize-afanasievbarbershop-608f.twc1.net']
 
 # Application definition
 
