6c543113fe3c60f4359e140dc8e21e6e20e9061e
Чекашов Матвей <chekashovmatvey@gmail.com>
2024-08-28 22:27:24 +0300

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 
 from users.models import User
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+  fields = [field.name for field in User._meta.get_fields()]
+  list_display = ('username')
+  list_display_links = ('username')
+  empty_value_display = '-пустой-'
+  list_per_page = 64
+  list_max_show_all = 8
+
+  def has_add_permission(self, request, obj=None):
+      return False
