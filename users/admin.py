f2ce06bf8e8c29be4826fd2504b98db8f7f6e774
Чекашов Матвей <chekashovmatvey@gmail.com>
2024-08-28 22:32:11 +0300

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -5,8 +5,8 @@ from users.models import User
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
   fields = [field.name for field in User._meta.get_fields()]
-  list_display = ('username')
-  list_display_links = ('username')
+  list_display = ('username',)
+  list_display_links = ('username',)
   empty_value_display = '-пустой-'
   list_per_page = 64
   list_max_show_all = 8
